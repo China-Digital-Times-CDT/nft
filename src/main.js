abd48157f0ecbe4ba07da088ac8aae347cd231e7
rkicoach <adamhereforwork@gmail.com>
2022-07-01 06:08:54 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// vue app
+const app = createApp(App);
+
+// bootstrap
+import "bootstrap"
+// vue select
+import vSelect from 'vue-select'
+import "vue-select/dist/vue-select.css";
+
+app.component('v-select', vSelect)
+
+// clipboard
+import VueClipboard from 'vue3-clipboard'
+app.use(VueClipboard, {
+    autoSetContainer: true,
+    appendToBody: true,
+})
+
+// template custom css
+import './assets/scss/bundles.scss'
+import './assets/scss/style.scss'
+
+
+
+// Global page components imported
+import HeaderMain from './components/common/HeaderMain.vue';
+import ButtonGroup from './components/common/ButtonGroup.vue';
+import Footer from './pages/Footer.vue';
+import FooterSection from './components/section/FooterSection.vue';
+
+// Global page components register
+app.component('HeaderMain', HeaderMain);
+app.component('ButtonGroup', ButtonGroup)
+app.component('Footer', Footer)
+app.component('FooterSection', FooterSection)
+// vue app
+app.use(router).mount('#app')
