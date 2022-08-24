621cd5faa4bb2f4bcafb900cc820639916b181c6
rkicoach <adamhereforwork@gmail.com>
2022-08-24 10:43:49 +0800

    #5

--- src/pages/LightningLogin.vue
+++ src/pages/LightningLogin.vue
@@ -7,8 +7,8 @@
     </header>
     <!-- login section -->
     <div class="text-center my-4">
-      <div v-if="lnurlValue != null">
-        <QrcodeVue :value="this.lnurlValue" :size="300"></QrcodeVue>
+      <div v-if="this.qrImg != null">
+        <!-- <QrcodeVue :value="this.lnurlValue" :size="300"></QrcodeVue>
         <div class="text-center">
           <input
             type="text"
@@ -16,7 +16,8 @@
             style="width: 700px; margin-top: 20px"
             v-model="lnurlValue"
           />
-        </div>
+        </div> -->
+        <a id="qrcode" href="#"><img v-bind:src="this.qrImg" /></a>
         <button
           type="button"
           id="lightning-uri-button"
@@ -37,7 +38,7 @@
 // Import component data. You can change the data in the store to reflect in all component
 import SectionData from "@/store/store.js";
 import axios from "axios";
-import QrcodeVue from "qrcode.vue";
+//import QrcodeVue from "qrcode.vue";
 // import { io } from 'socket.io-client';
 
 export default {
@@ -52,24 +53,44 @@ export default {
       lnurlValue: null,
       key: null,
       statusShedule: null,
+      qrImg: null,
     };
   },
   components: {
-    QrcodeVue,
+    //  QrcodeVue,
   },
   async mounted() {
     console.log("code---", this.$route.query.code);
+    // await axios
+    //   .get("https://gem.chinadigitaltimes.net/api/user/lnurl")
+    //   .then(async (response) => {
+    //     console.log("api--response---", response.data);
+    //     localStorage.setItem("authId", response.data.token);
+    //     console.log("Successfully logged in--token!", response.token);
+    //     this.lnurlValue = response.data.encoded;
+    //   })
+    //   .catch((error) => console.log(error));
+
+    // this.statusShedule = setInterval(this.getUStatus, 1000);
     await axios
-      .get("https://gem.chinadigitaltimes.net/api/user/lnurl")
+      .get("https://gem.chinadigitaltimes.net/api/user/login")
       .then(async (response) => {
         console.log("api--response---", response.data);
-        localStorage.setItem("authId", response.data.token);
-        console.log("Successfully logged in--token!", response.token);
-        this.lnurlValue = response.data.encoded;
+        //let html = response.data;
+        //console.log("href---", JSON.parse(response.data));
+
+        var el = document.createElement("html");
+        el.innerHTML = response.data.toString();
+        var aaa = el.getElementsByTagName("a");
+        var img = el.getElementsByTagName("img");
+        this.qrImg = img[0].src;
+
+        console.log("222222222", aaa[0].href);
+        console.log("333333333", this.qrImg);
       })
       .catch((error) => console.log(error));
 
-    this.statusShedule = setInterval(this.getUStatus, 1000);
+    //   this.statusShedule = setInterval(this.getUStatus, 1000);
   },
 
   methods: {
