ab27ee24dce43262afc7ab776ca097ecdb13e56f
rkicoach <adamhereforwork@gmail.com>
2022-09-02 11:47:05 +0800

    #8,

--- src/components/section/WalletSection.vue
+++ src/components/section/WalletSection.vue
@@ -349,7 +349,7 @@ export default {
         })
         .then((response) => {
           console.log("api--response---update--user", response.data);
-          //this.invoiceValue = null;
+          this.invoiceValue = null;
           this.amount = 0;
           this.withdraw_status = false;
           this.paid = false;
